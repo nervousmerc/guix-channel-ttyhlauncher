b5aea489363a8ee881e6b6447a89dbc1b586aae3
Constantine <nervousmerc@gmail.com>
2019-11-14 19:27:07 +0500

    add news file

---
+++ news.scm
@@ -0,0 +1,7 @@
+(channel-news
+ (version 0)
+ (entry (commit "38403417e7063df3bb3c96509705f50c59e980b4")
+        (title (en "Ttyh! Launcher package added")
+               (ru "Добавлен пакет Ttyh! Launcher"))
+        (body (en "Package name is @code{ttyhlauncher}.")
+              (ru "Имя пакета @code{ttyhlauncher}."))))
